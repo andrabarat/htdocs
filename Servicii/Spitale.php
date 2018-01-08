4725b6b7c910a8aabcf8799e6eaf1bc334a736d4
andrabarat <34458119+andrabarat@users.noreply.github.com>
2018-01-08 23:57:35 +0200

    Modified "Servicii" page

--- Servicii/Spitale.php
+++ Servicii/Spitale.php
@@ -2,15 +2,6 @@
     include "../global/session.php";
     include "../global/header.php";
     include "../global/dbConnect.php";
-
-    $id_session=0;
-    $sql = "SELECT id_user FROM users WHERE user_name='".$login_session."'";
-    $result = $conn->query($sql);        
-    if ($result->num_rows > 0){
-        while($row = $result->fetch_assoc()) {
-            $id_session=$row["id_user"];
-        }
-    }
 ?>
 
 <html>
